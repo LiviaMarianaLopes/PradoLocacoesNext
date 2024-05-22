de9a5cacdaac351be96f2c9b9da4e00b1a7f3a4e
Lívia Mariana Lopes <liviamariana2004@gmail.com>
2024-05-22 11:58:26 -0300

    :sparkles: feat: Componente Footer

--- src/components/footer/index.tsx
+++ src/components/footer/index.tsx
@@ -1,10 +1,43 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import "./styles.css";
 import Image from "next/image";
 import Link from "next/link";
+import {
+  faFacebook,
+  faInstagram,
+  faWhatsapp,
+} from "@fortawesome/free-brands-svg-icons";
+import { faEnvelope, faVoicemail } from "@fortawesome/free-solid-svg-icons";
 const Footer = () => {
   return (
-    <>
-    </>
+    <footer>
+      <section>
+        <p>
+          A Prado Locações é especialista em locações de estruturas para Shows e
+          Eventos. Faça um orçamento sem compromisso!
+        </p>
+      </section>
+      <section className="socials">
+        <a className="social-link" href="https://wa.me/5511974592264">
+          <FontAwesomeIcon icon={faWhatsapp} />
+        </a>
+        <a
+          className="social-link"
+          href="https://www.facebook.com/pradoeventos/"
+        >
+          <FontAwesomeIcon icon={faFacebook} />
+        </a>
+        <a
+          className="social-link"
+          href="https://www.instagram.com/pradolocacoes/"
+        >
+          <FontAwesomeIcon icon={faInstagram} />
+        </a>
+        <a className="social-link" href="mailto:prado.locacoes@gmail.com">
+          <FontAwesomeIcon icon={faEnvelope} />
+        </a>
+      </section>
+    </footer>
   );
 };
 
